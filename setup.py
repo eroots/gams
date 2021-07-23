f367a08dc84d608f285fc7852d4129c8a97a5771
Eric Roots <eroots087@gmail.com>
2021-07-23 15:22:19 -0400

    UI change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='gams',
-      version='0.0.5',
+      version='0.0.6',
       description='GUI tool providing various transformations of magnetic data',
       url='http://github.com/eroots/gams',
       author='Eric Roots',
